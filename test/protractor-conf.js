501714c8a74980916dbe1e24427f578407bd0b28
futur <futur.talk@me.com>
2016-04-09 11:28:48 +0530

    Initial travis checkin

--- test/protractor-conf.js
+++ test/protractor-conf.js
@@ -19,7 +19,18 @@ exports.config = {
   // or configure a single browser
   
   capabilities: {
-    'browserName': 'chrome'
+    'browserName': 'phantomjs',
+
+  /* 
+   * Can be used to specify the phantomjs binary path.
+   * This can generally be ommitted if you installed phantomjs globally.
+   */
+
+  /*
+   * Command line args to pass to ghostdriver, phantomjs's browser driver.
+   * See https://github.com/detro/ghostdriver#faq
+   */
+  'phantomjs.ghostdriver.cli.args': ['--loglevel=DEBUG']
   },
   
 
